6669e5791199bc26d6ac61ea89a76caf4dc66ecc
Gab Labelle <gab@visualise.ca>
2019-02-22 20:46:43 -0500

    Updated comment

--- packages/universal/src/index.tsx
+++ packages/universal/src/index.tsx
@@ -1,4 +1,2 @@
+// Export all your components
 export { default as Welcome } from './components/Welcome';
-// export {default as Comp1} from './components/Comp1.tsx';
-// export {default as Comp2} from './components/Comp2.tsx';
-// export {default as Comp3} from './components/Comp3.tsx';
