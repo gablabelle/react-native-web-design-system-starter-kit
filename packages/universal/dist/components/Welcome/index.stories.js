2e2a611758f75b26cd372df080149528233d3911
Gab Labelle <gab@visualise.ca>
2019-02-22 23:59:54 -0500

    Added cross-env SKIP_PREFLIGHT_CHECK

--- packages/universal/dist/components/Welcome/index.stories.js
+++ packages/universal/dist/components/Welcome/index.stories.js
@@ -5,5 +5,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = require("@storybook/react");
 var react_2 = __importDefault(require("react"));
-var Welcome_1 = __importDefault(require("./Welcome"));
-react_1.storiesOf('Welcome', module).add('to React Native Web', function () { return react_2.default.createElement(Welcome_1.default, null); });
+var _1 = __importDefault(require("."));
+react_1.storiesOf('Welcome', module).add('to React Native Web', function () { return react_2.default.createElement(_1.default, null); });
