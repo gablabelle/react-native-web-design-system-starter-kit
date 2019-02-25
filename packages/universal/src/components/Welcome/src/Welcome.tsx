6e325b29501a235a80b7fde341a4b2cbcc1597d3
Gab Labelle <gab@visualise.ca>
2019-02-24 22:32:03 -0500

    Now shipping compiled code

--- packages/universal/src/components/Welcome/src/Welcome.tsx
+++ packages/universal/src/components/Welcome/src/Welcome.tsx
@@ -26,7 +26,6 @@ const Welcome = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.tsx</Text>
       <Text style={styles.instructions}>{count}</Text>
       <Button title="increment" onPress={() => setCount(count + 1)} />
     </View>
