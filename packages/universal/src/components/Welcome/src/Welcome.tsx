b9cae6c4ec889b67f08ec6846b1cff27e8efc72b
Gab Labelle <gab@visualise.ca>
2019-11-03 14:51:59 -0500

    Watching files

--- packages/universal/src/components/Welcome/src/Welcome.tsx
+++ packages/universal/src/components/Welcome/src/Welcome.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 import styled from 'styled-components/native';
 
 const StyledText = styled.Text`
-  background-color: green;
-  color: red;
-  position: absolute;
-  z-index: 1;
+  background-color: gray;
+  color: black;
 `;
 
 function Welcome() {
